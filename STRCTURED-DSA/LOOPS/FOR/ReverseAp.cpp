89c5aa0abb8583ddb56543eff2b12380be870731
Umesh Jadhav <umeshrajput24196@gmail.com>
2026-08-05 10:08:44 +0530

    Reverse AP

---
+++ STRCTURED-DSA/LOOPS/FOR/ReverseAp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //series = 100 97 94 ...7 4 1
+    
+    for(int i=100; i>0; i-=3){
+
+        cout<<i<<endl;
+       
+
+    }
+
+
+    
+}
