23ed6377bf0b52fc08c5576af0d2372e235973eb
Umesh Jadhav <umeshrajput24196@gmail.com>
2026-08-05 09:13:07 +0530

    n times hello print

---
+++ STRCTURED-DSA/LOOPS/FOR/helloworld.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of time you want to print : ";
+    cin>>n;
+
+    for(int i = 1; i<=n ; i++){
+        cout<<"Hello World"<<endl;
+    }
+}
