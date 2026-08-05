f529e554c63b410f3449db651e3455fec279351d
Umesh Jadhav <umeshrajput24196@gmail.com>
2026-08-05 09:53:55 +0530

    print AP Series

---
+++ STRCTURED-DSA/LOOPS/FOR/AP.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    //series = 1 3 5 7 9...
+    int n;
+    cout<<"enter number : ";
+    cin>>n;
+
+    int a = 1;
+    for(int i = 1; i<=n; i++){
+
+        cout<<a<<endl;
+        a = a + 2;
+
+    }
+
+
+    
+}
