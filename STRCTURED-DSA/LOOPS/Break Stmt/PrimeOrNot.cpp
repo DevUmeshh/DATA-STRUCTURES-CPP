156177e2cfcfffe46b7b0b4c18427261f595a7d6
Umesh Jadhav <umeshrajput24196@gmail.com>
2026-08-06 09:49:14 +0530

    Prime  or not

---
+++ STRCTURED-DSA/LOOPS/Break Stmt/PrimeOrNot.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //composite numbers - the number having factors means divisible by 1 and its factors...
+    int n;
+    cout<<"Enter A Number : ";
+    cin>>n;
+    bool flag = true; //its prime
+    for(int i=2; i<=n-1; i++){
+
+        if(n%i==0){
+            flag = false;
+            break;
+        }
+        
+    }
+    if(n==1) cout<<"1 is neither prime not composite";
+        else if(flag==true) cout<<n<<" is prime";
+        else cout<<n<<" is composite";
+
+}
